6577839dc8577737a3936a8019b3e9590f41a378
asjadzaki <asjadzaki2021@gmail.com>
2024-12-24 07:51:35 +0530

    Save data and generate html for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -12,7 +12,7 @@ export const deliveryOptions = [{
     priceCents: 999
   }];
   
-  export function getDeliveryOption(deliveryOptionId) {
+export function getDeliveryOption(deliveryOptionId) {
     let deliveryOption;
   
     deliveryOptions.forEach((option) => {
